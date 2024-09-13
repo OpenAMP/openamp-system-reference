c3782b302818865135ef1cee048ad06183cbd87b
Iuliana Prodan <iuliana.prodan@nxp.com>
2024-09-13 19:04:01 +0300

    examples: zephyr: increase stack size
    While testing rpmsg_multi_services example on HiFi4 DSP from
    i.MX8M Plus, realized the stack is not enough.
    Increase the size based on Thread Analyzer measurements:
    ...
    <dbg> openamp_rsc_table: mailbox_notify: mailbox_notify: msg received
    
    OpenAMP Linux sample client responder ended
    Thread analyze:
     0x9240d5b0          : STACK: unused 688 usage 1360 / 2048 (66 %); CPU: 0 %
          : Total CPU cycles used: 25065
     0x9240d638          : STACK: unused 704 usage 1344 / 2048 (65 %); CPU: 0 %
          : Total CPU cycles used: 19662
     0x9240d748          : STACK: unused 328 usage 696 / 1024 (67 %); CPU: 0 %
          : Total CPU cycles used: 1234863
     0x9240d7d0          : STACK: unused 272 usage 752 / 1024 (73 %); CPU: 0 %
          : Total CPU cycles used: 53216
     0x9240d858          : STACK: unused 168 usage 856 / 1024 (83 %); CPU: 98 %
          : Total CPU cycles used: 136995186
     0x9240d920          : STACK: unused 936 usage 88 / 1024 (8 %); CPU: 0 %
          : Total CPU cycles used: 0
     ISR0                : STACK: unused 1584 usage 464 / 2048 (22 %)
    
    Signed-off-by: Iuliana Prodan <iuliana.prodan@nxp.com>

--- examples/zephyr/rpmsg_multi_services/src/main_remote.c
+++ examples/zephyr/rpmsg_multi_services/src/main_remote.c
@@ -31,14 +31,13 @@ LOG_MODULE_REGISTER(openamp_rsc_table, LOG_LEVEL_DBG);
 #define SHM_START_ADDR	DT_REG_ADDR(SHM_NODE)
 #define SHM_SIZE		DT_REG_SIZE(SHM_NODE)
 
-#define APP_TASK_STACK_SIZE (512)
+#define APP_TASK_STACK_SIZE (1024)
 
 #define MAX_TTY_EPT  2
 #define MAX_RAW_EPT  2
 
-/* Add 512 extra bytes for the TTY task stack for the "tx_buff" buffer. */
-#define APP_TTY_TASK_STACK_SIZE (1024)
-#define APP_RAW_TASK_STACK_SIZE (1024)
+#define APP_TTY_TASK_STACK_SIZE (2048)
+#define APP_RAW_TASK_STACK_SIZE (2048)
 
 K_THREAD_STACK_DEFINE(thread_mng_stack, APP_TASK_STACK_SIZE);
 K_THREAD_STACK_DEFINE(thread_rp__client_stack, APP_TASK_STACK_SIZE);
